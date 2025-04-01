9e0cf9fec4aab85d3eb9c1453369cdcb08ea4f7d
IsaacLaw20 <139254052+IsaacLaw20@users.noreply.github.com>
2025-04-01 17:09:03 +0100

    new data added

--- Script5.m
+++ Script5.m
@@ -1,23 +1,20 @@
 % Create the data matrix: columns correspond to [Iload (mA), R, f]
 data = [ ...
-    3.22, 11,   100e3;
-    3.00, 30,   100e3;
-    2.90, 63,   100e3;
-    2.60, 100,  100e3;
-    2.15, 220,  100e3;
-    1.91, 300,  100e3;
-    3.30, 11,    50e3;
-    3.00, 30,    50e3;
-    2.90, 63,    50e3;
-    2.77, 100,   50e3;
-    2.22, 220,   50e3;
-    1.97, 300,   50e3;
-    2.28, 11,   150e3;
-    2.21, 30,   150e3;
-    1.98, 63,   150e3;
-    1.82, 100,  150e3;
-    1.61, 220,  150e3;
-    1.46, 300,  150e3];
+    10,    300,   50e3;
+    11.8,  220,   50e3;
+    20,    110,   50e3;
+    28,    62,    50e3;
+    64,    10,    50e3;
+    12,    300,  100e3;
+    17.3,  220,  100e3;
+    30,    110,  100e3;
+    40,    62,   100e3;
+    100,   10,   100e3;
+    17,    300,  150e3;
+    23.8,  220,  150e3;
+    40,    110,  150e3;
+    60,    62,   150e3;
+    160,   10,   150e3 ];
 
 % Separate the data based on frequency
 freq100 = data(data(:,3) == 100e3, :);
@@ -26,13 +23,13 @@ freq150 = data(data(:,3) == 150e3, :);
 
 % Create the plot
 figure;
-plot(freq100(:,2), freq100(:,1), '-o', 'DisplayName', '100 kHz'); hold on;
-plot(freq50(:,2),  freq50(:,1),  '-s', 'DisplayName', '50 kHz');
-plot(freq150(:,2), freq150(:,1), '-d', 'DisplayName', '150 kHz'); hold off;
+plot(freq150(:,2), freq150(:,1), '-d', 'DisplayName', '150 kHz'); hold on;
+plot(freq100(:,2), freq100(:,1), '-o', 'DisplayName', '100 kHz');
+plot(freq50(:,2),  freq50(:,1),  '-s', 'DisplayName', '50 kHz'); hold off;
 
 % Labeling the axes and adding title
 xlabel('Resistance (Ω)');
 ylabel('Load Current (mA)');
 
-legend('Location','Best');
+legend({'150 kHz', '100 kHz', '50 kHz'}, 'Location', 'Best');
 grid on;
